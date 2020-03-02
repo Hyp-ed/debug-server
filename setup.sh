c6e7ffce6b416aed09f7a14d0cc28173e184508e
Paul Martin <paul@blibspace.com>
2020-03-02 20:17:28 +0000

    Change hyped-2020 submodule branch to develop

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+BRANCH="develop"
+
 # Root directory of repo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 git submodule init
 git submodule update --merge
+(cd $DIR/hyped-pod_code && git checkout $BRANCH)
 (cd $DIR/server && npm install)
