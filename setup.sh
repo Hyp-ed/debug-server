a22d1470daa55c0e8375c308072b7f3e18f4d13b
Paul Martin <paul@blibspace.com>
2020-03-09 19:18:38 +0000

    Use yarn instead of npm

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 git submodule init
 git submodule update --merge
 (cd $DIR/hyped-pod_code && git checkout $BRANCH)
-(cd $DIR/server && npm install)
+(cd $DIR/server && yarn install)
